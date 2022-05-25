704d20878121cf0fa83ca528c888a128543f6e69
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-25 17:11:54 +0200

    installato css e creazione layout

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,10 +1,10 @@
-@extends('layout.app')
+@extends('/app')
 @section('content')
     <h1>Lista fumetti:</h1>
-    <div class="flex">
+    <div class="d-flex flex-wrap justify-content-center gap-2">
         @foreach ($comics as $comic)
             <div class="card">
-                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                <img class="w-100" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
                 <h3>Titolo: </h3>
                 <p>{{ $comic->title }}</p>
                 <h5>Serie: {{ $comic->series }}</h5>
